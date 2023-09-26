09a0e5fb7e02136957c8b70da648ebbeb6ab060f
Brodie Ashcraft <brodiea19@gmail.com>
2023-09-25 19:55:43 -0700

    added signup, login/logout views

--- server/app.py
+++ server/app.py
@@ -14,12 +14,20 @@ class UserSchema(ma.SQLAlchemySchema):
     placed_order = ma.Nested("WorkSchema", many=True, exclude=("requested_by",))
 
 
+single_user_schema = UserSchema()
+plural_user_schema = UserSchema(many=True)
+
+
 class EmployeeSchema(ma.SQLAlchemySchema):
     class Meta:
         model = Employee
         exclude = ("_password_hash",)
 
     work_order = ma.Nested("WorkSchema", many=True, exclude=("assigned_to",))
+
+
+single_emp_schema = EmployeeSchema()
+plural_emp_schema = EmployeeSchema(many=True)
 
 
 class WorkSchema(ma.SQLAlchemyAutoSchema):
@@ -29,6 +37,58 @@ class WorkSchema(ma.SQLAlchemyAutoSchema):
     requested_by = ma.Nested("UserSchema", only=("id", "name", "email"))
     assigned_to = ma.Nested("EmployeeSchema", only=("id", "name", "username"))
 
+
+single_work_schema = WorkSchema()
+plural_work_schema = WorkSchema(many=True)
+
+# ######## #
+# DA VIEWS #
+# ######## #
+
+
+class Signup(Resource):
+    def post(self):
+        json = request.get_json()
+
+        if not json.get("email"):
+            return {"message": "invalid email"}, 422
+
+        user = User(name=json.get("name"), email=json.get("email"))
+        user.password_hash = json.get("password")
+
+        if not user:
+            return {"message": "invalid user info"}, 422
+
+        db.session.add(user)
+        db.session.commit()
+
+        session["user_id"] = user.id
+        return single_user_schema.dump(user), 201
+
+
+class Login(Resource):
+    def post(self):
+        user_info = request.get_json()
+
+        user = User.query.filter(User.email == user_info.get("email")).first()
+
+        if user and user.authenticate(user_info.get("password")):
+            session["user_id"] = user.id
+            return single_user_schema.dump(user), 200
+
+        return {}, 401
+
+
+class Logout(Resource):
+    def delete(self):
+        session["user_id"] = None
+
+        return {}, 401
+
+
+api.add_resource(Login, "/login")
+api.add_resource(Logout, "logout")
+api.add_resource(Signup, "/signup")
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
