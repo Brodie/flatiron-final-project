fa59d6bd7ff2da06273410005f15059e7e6a1e3a
Brodie Ashcraft <brodiea19@gmail.com>
2023-09-25 19:33:39 -0700

    added complete/completed_at columns to work model. tested db

--- server/models.py
+++ server/models.py
@@ -52,6 +52,7 @@ class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     username = db.Column(db.String, unique=True)
+    admin = db.Column(db.Boolean, default=False)
     _password_hash = db.Column(db.String)
     work_order = db.relationship("Work", back_populates="assigned_to")
 
@@ -86,10 +87,22 @@ class Work(db.Model):
     requested_by = db.relationship("User", back_populates="placed_order")
     assigned_to = db.relationship("Employee", back_populates="work_order")
     created_at = db.Column(db.DateTime, server_default=db.func.now())
-    # add way to have completed at column that updates when completed is updated
-
+    completed = db.Column(db.Boolean, default=False)
+    completed_at = db.Column(db.DateTime)
     employee_id = db.Column(db.Integer, db.ForeignKey("employees.id"))
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    @hybrid_property
+    def complete(self):
+        return self.completed
+
+    @complete.setter
+    def complete(self, complete):
+        if complete and not self.complete:
+            self.completed = True
+            self.completed_at = db.func.now()
+        if not complete and self.complete:
+            self.completed_at = None
 
     def __repr__(self):
         return f"<Work Order ID:{self.id}>"
