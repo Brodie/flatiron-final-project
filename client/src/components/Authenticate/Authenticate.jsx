7ac5a33b07644933dcc8bf0aab5c0f80959406cf
Brodie Ashcraft <brodiea19@gmail.com>
2023-09-28 10:25:35 -0700

    added formik and yup. created yup schema, added initial formik values

--- client/src/components/Authenticate/Authenticate.jsx
+++ client/src/components/Authenticate/Authenticate.jsx
@@ -1,9 +1,36 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 function Authenticate() {
   const location = useLocation();
   const { isEmployee } = location.state;
+
+  const formSchema = yup.object().shape({
+    email: yup.string().email("Invalid email").required("Must enter email"),
+    name: yup.string().required("Name required"),
+    username: yup.string().required("Username required"),
+    password: yup.string().required("Password required"),
+    passConfirm: yup
+      .string()
+      .oneOf([yup.ref("password")], "Passwords must match"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      name: "",
+      username: "",
+      password: "",
+      passConfirm: "",
+    },
+    validationSchema: formSchema,
+    onSubmit: (values) => {
+      fetch("/login");
+    },
+  });
+
   return <div>Authenticate: {isEmployee ? "true" : "false"}</div>;
 }
 
