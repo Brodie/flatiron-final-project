e63c1628d9831f7d968305ab13e09e401ab2d75d
Brodie Ashcraft <brodiea19@gmail.com>
2023-09-28 10:50:29 -0700

    built out form fetch with error catching

--- client/src/components/Authenticate/Authenticate.jsx
+++ client/src/components/Authenticate/Authenticate.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
 import * as yup from "yup";
 
 function Authenticate() {
   const location = useLocation();
   const { isEmployee } = location.state;
+  const navigate = useNavigate();
 
   const formSchema = yup.object().shape({
     email: yup.string().email("Invalid email").required("Must enter email"),
@@ -27,7 +28,21 @@ function Authenticate() {
     },
     validationSchema: formSchema,
     onSubmit: (values) => {
-      fetch("/login");
+      fetch("/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // add null and 2 for readability when viewing data
+        body: JSON.stringify(values, null, 2),
+      }).then((res) => {
+        if (res.ok) {
+          res.json().then((user) => {
+            updateUser(user);
+            navigate("/");
+          });
+        }
+      });
     },
   });
 
