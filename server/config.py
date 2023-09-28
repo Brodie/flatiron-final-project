9707833be0dd2301a4f5bc652dd7eeeaeb346628
Brodie Ashcraft <brodiea19@gmail.com>
2023-09-28 12:59:23 -0700

    added secret key config

--- server/config.py
+++ server/config.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
@@ -5,12 +6,11 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_marshmallow import Marshmallow
-from 'client/.secret' import key
 
 app = Flask(__name__)
 
 
-app.secret_key = "yo mama"
+app.secret_key = os.environ.get("APP_SECRET_KEY")
 
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
