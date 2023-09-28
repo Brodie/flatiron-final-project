107475d0e086f0699e986c1aaed51479c27daca8
Brodie Ashcraft <brodiea19@gmail.com>
2023-09-28 12:55:56 -0700

    test

--- server/config.py
+++ server/config.py
@@ -5,12 +5,12 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_marshmallow import Marshmallow
+from 'client/.secret' import key
 
 app = Flask(__name__)
 
-# change this before deploying
-#
-# app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+
+app.secret_key = "yo mama"
 
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
