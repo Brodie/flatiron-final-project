34ebe3594568316d16bf1fca61805f24bbed8933
Brodie Ashcraft <brodiea19@gmail.com>
2023-09-28 23:02:39 -0700

    began building form

--- client/src/components/WorkForm/WorkForm.jsx
+++ client/src/components/WorkForm/WorkForm.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
-function WorkForm() {
+function WorkForm({ user }) {
+  const formSchema = yup.object().shape({
+    info: yup.string().required("Please Include Info"),
+  });
+
   return <div>WorkForm</div>;
 }
 
