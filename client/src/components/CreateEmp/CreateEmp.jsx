4f3e815cbde6afa8456b15f5861d1efafc92a38c
Brodie Ashcraft <brodiea19@gmail.com>
2023-10-12 09:56:26 -0700

    Created Employee Creation Component for Admin use

---
+++ client/src/components/CreateEmp/CreateEmp.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./CreateEmp.css";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+function CreateEmp() {
+  return <div>CreateEmp</div>;
+}
+
+export default CreateEmp;
