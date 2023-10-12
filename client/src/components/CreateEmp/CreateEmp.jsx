9ddb303406b42903a7fdfd30ed62013aca003910
Brodie Ashcraft <brodiea19@gmail.com>
2023-10-12 10:15:33 -0700

    created route for Employee Creation, Created form schema

--- client/src/components/CreateEmp/CreateEmp.jsx
+++ client/src/components/CreateEmp/CreateEmp.jsx
@@ -3,7 +3,29 @@ import "./CreateEmp.css";
 import { useFormik } from "formik";
 import * as yup from "yup";
 
-function CreateEmp() {
+function CreateEmp({ emp }) {
+  const formSchema = yup.object().shape({
+    username: yup.string().required("Please Include Username"),
+    name: yup.string().required("Please Enter Name"),
+    password: yup.string().required("Password Required"),
+    passConfirm: yup
+      .string()
+      .oneOf([yup.ref("password"), "Passwords must match"]),
+    admin: yup.bool(),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      name: "",
+      password: "",
+      passConfirm: "",
+      admin: false,
+    },
+    validationSchema: formSchema,
+    onSubmit: (values) => {},
+  });
+
   return <div>CreateEmp</div>;
 }
 
